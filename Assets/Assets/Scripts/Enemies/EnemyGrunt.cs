6bb1d530c13d13c3e4846a7900e58c729d0e76b8
propredator23 <christiandash@hotmail.com>
2020-08-11 14:57:50 -0700

    Enemies and Movement
    Added the basic enemy and movement

---
+++ Assets/Assets/Scripts/Enemies/EnemyGrunt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGrunt : Enemy
+{
+    public Animation animations;
+    public AnimationClip flying;
+    public AnimationClip flyIn;
+    private Vector3 start;
+    private float waitTime;
+    private bool isflying;
+    System.Random rng = new System.Random();
+
+    private void Start()
+    {
+        animations.Play(flyIn.name);
+        waitTime = flyIn.length;
+    }
+    private void FixedUpdate()
+    {
+        waitTime -= Time.deltaTime;
+        if (waitTime <= 0)
+        {
+            start = transform.localPosition;
+            animations.Play(flying.name);
+            isflying = true;
+        }
+    }
+    private void Update()
+    {
+        if(isflying)transform.localPosition = Vector3.Lerp(start,start+new Vector3(-25f,0,0),1);
+    }
+}
