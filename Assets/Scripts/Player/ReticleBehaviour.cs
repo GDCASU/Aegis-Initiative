f423e4e251ec64d690eb825460fc15fb11a4c3d6
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-09-16 14:35:13 -0700

    Added second reticle

--- Assets/Scripts/Player/ReticleBehaviour.cs
+++ Assets/Scripts/Player/ReticleBehaviour.cs
@@ -7,7 +7,6 @@ public class ReticleBehaviour : MonoBehaviour
 {
     public Camera playerCam;
     public Transform shipWorldReference;    //This is a gameobject within the 3D world that I can use to make a positional reference to the front of the ship
-    public Transform innerReticle;
 
     private RectTransform rectTransform;
     private float imageWidth;
