4723006941d4fbf67500e050629df7b9cfb213b5
propredator23 <christiandash@hotmail.com>
2020-09-25 16:22:55 -0700

    BasicShooting
    Added the basic shooting mechanic to the game

---
+++ Assets/Scripts/Player/BasicPlayerShooting.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerShooting : MonoBehaviour
+{
+    public float fireRate;
+    public float speed;
+    public int magSize;
+    public GameObject bulletPrefab;
+    public GameObject bullet;
+    public Transform spawnR;
+    public Transform spawnL;
+    public bool alternate;
+
+    private float timerOne;
+    private float timerTwo;
+
+    private void Start()
+    {
+        timerOne = fireRate;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (alternate)
+            {
+                if (timerOne < 0)
+                {
+                    bullet = Instantiate(bulletPrefab, spawnR.position, spawnR.rotation);
+                    bullet.GetComponent<Rigidbody>().velocity = spawnR.forward * speed;
+                    timerOne = fireRate;
+                    timerTwo = fireRate / 2f;
+                }
+                if (timerTwo < 0)
+                {
+                    bullet = Instantiate(bulletPrefab, spawnL.position, spawnL.rotation);
+                    bullet.GetComponent<Rigidbody>().velocity = spawnL.forward * speed;
+                    timerTwo = fireRate;
+                }
+            }
+            else 
+            {
+                if (timerOne < 0)
+                {
+                    bullet = Instantiate(bulletPrefab, spawnR.position, spawnR.rotation);
+                    bullet.GetComponent<Rigidbody>().velocity = spawnR.forward * speed;
+                    bullet = Instantiate(bulletPrefab, spawnL.position, spawnL.rotation);
+                    bullet.GetComponent<Rigidbody>().velocity = spawnL.forward * speed;
+                    timerOne = fireRate;
+                }
+            }          
+        }
+        timerOne -= Time.deltaTime;
+        timerTwo -= Time.deltaTime;
+    }
+}
