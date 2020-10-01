6231f6b4d8b502770c3ddfe054c518db9d774fb8
Phyrol <nitehawkk123@gmail.com>
2020-09-30 18:18:55 -0700

    EnemySpawner and Basic Movement
    - Enemy spawner complete
    
    - basic movement started (currently move forward)

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,11 +1,50 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public int grunts;
-    public GameObject gruntPrefab;
+    public enum spawnLocation
+    {
+        TopLeft,
+        TopMiddle,
+        TopRight,
+        MiddleLeft,
+        MiddleRight,
+        BottomLeft,
+        BottomRight
+    }
+
+    [Serializable]
+    public struct enemies //struct for which enemies to spawn
+    {
+        public spawnLocation SpawnLocation;
+        public GameObject enemyPrefab; //enemy prefab
+    }
+
+    [SerializeField]
+    public enemies[] _enemies; //array of enemy struct
+
+    private float adjustLocation; //adjust spawning around DollyCart box collider size.
+    private float initialX;
+    private float initialY;
+    private float xSpawnAdjustment;
+    private float ySpawnAdjustment;
+    private float zSpawnAdjustment;
+    private Vector3 spawnAdjustment;
+
+    private void Start()
+    {
+        Vector3 temp = GameObject.Find("GameDollyCart").GetComponent<BoxCollider>().size; //grab collider size
+        adjustLocation = 2.0f;
+
+        initialX = temp.x * adjustLocation;
+        initialY = temp.y * adjustLocation;
+        zSpawnAdjustment = -2.0f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //print(other.name);
@@ -14,10 +53,38 @@ public class EnemySpawner : MonoBehaviour
             //if (//other.GetComponent<PlayerInfo>().enemiesOnScreen == 0)
             //{
             //other.GetComponent<PlayerInfo>().enemiesOnScreen--;
-            for (int x = 0; x < grunts; x++)
+            for (int x = 0; x < _enemies.Length; x++) //loop through enemies to spawn
+            {
+                xSpawnAdjustment = initialX;
+                ySpawnAdjustment = initialY;
+                switch (_enemies[x].SpawnLocation) //chance x and/or y adjustment based on selected location
                 {
-                    Instantiate(gruntPrefab, other.gameObject.transform.position, Quaternion.identity, other.gameObject.transform);
+                    case spawnLocation.TopLeft:
+                        xSpawnAdjustment *= -1.0f;
+                        break;
+                    case spawnLocation.TopMiddle:
+                        xSpawnAdjustment = 0.0f;
+                        break;
+                    case spawnLocation.MiddleLeft:
+                        xSpawnAdjustment *= -1.0f;
+                        ySpawnAdjustment = 0.0f;
+                        break;
+                    case spawnLocation.MiddleRight:
+                        ySpawnAdjustment = 0.0f;
+                        break;
+                    case spawnLocation.BottomLeft:
+                        xSpawnAdjustment *= -1.0f;
+                        ySpawnAdjustment *= -1.0f;
+                        break;
+                    case spawnLocation.BottomRight:
+                        ySpawnAdjustment *= -1.0f;
+                        break;
+                    default:
+                        break;
                 }
+                spawnAdjustment = new Vector3(xSpawnAdjustment, ySpawnAdjustment, zSpawnAdjustment); //spawn adjustment vector to add to other.gameObject.transform.position
+                Instantiate(_enemies[x].enemyPrefab, other.gameObject.transform.position + spawnAdjustment, other.transform.rotation, other.gameObject.transform);
+            }
             //}
         }
     }
