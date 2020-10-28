c461f2e2d0590340a204e279728e6a02633b97b8
Thy Do <thydo@asu.edu>
2020-10-27 21:28:46 -0700

    Revert "Changed enemies script to destroy them after receiving damage"
    This reverts commit 58f4204b6b1b2d7e72eaf01efb9cbf7dab75312a.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    float health;
     GameObject prefab;
 
     public void OnCollisionEnter(Collision collision)
@@ -12,8 +13,6 @@ public class Enemy : MonoBehaviour
         {
             //health -= collision.gameObject.GetComponent<PlayerShot>().damage;
         }
-        
     }
-    
 
 }
