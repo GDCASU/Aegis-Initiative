697359dee19244c7a6f66d8ebb3c5e96bfbc8af1
Thy Do <thydo@asu.edu>
2020-10-30 05:01:35 -0700

    Added enemy health script, enemy dies when health is beneath 0

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    int Health;
+    public int DamageRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 50;
+        DamageRate = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "BulletPrefab(Clone)")
+        {
+            Health -= DamageRate;
+        }
+    }
+}
