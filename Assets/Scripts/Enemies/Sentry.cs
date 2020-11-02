fa1cb71eee6465087ceac75c288b4ec50227fe1d
danielwaker <dver99@sbcglobal.net>
2020-11-02 01:55:12 -0700

    Sentry
    added sentry

---
+++ Assets/Scripts/Enemies/Sentry.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sentry : Enemy
+{
+    public GameObject bulletPrefab;
+    private GameObject player;
+    private Transform gun;
+    private float bulletSpeed = 0.25f;
+
+    void Start()
+    {
+        gun = transform.GetChild(1).transform;
+        StartCoroutine("Shoot");
+    }
+
+    IEnumerator Shoot()
+    {
+        while (GetComponent<Renderer>().isVisible)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, gun.position, gun.rotation, transform);
+            bullet.GetComponent<Rigidbody>().AddForce((player.transform.position - gun.position) * 200f);
+            yield return new WaitForSeconds(bulletSpeed);
+        }
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+}
---
+++ Assets/Scripts/Enemies/SentryTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentryTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "GameDollyCart")
+        {
+            transform.parent.GetComponent<Sentry>().enabled = true;
+            transform.parent.GetComponent<Sentry>().SetPlayer(collider.transform.GetChild(1).gameObject);
+        }
+    }
+}
