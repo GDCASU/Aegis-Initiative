144a7e71f1a55f6e10fdddefff7ee58807b28958
propredator23 <christiandash@hotmail.com>
2020-11-02 02:42:40 -0700

    Asteroids
    Added an asteroid spawner to spwan asteroodis on a scene and move the asteroids, the asteroids will spawn based somewhere inside the box collider so that should be used to exedns the area where asteroids are wanted to be spawn, the sizem health, and speed at wich the asteroids sapwn is editable from the asteroidshandler script

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 origin;
+    public int health;
+    private BoxCollider bc;
+    public Vector2 velocityRange;
+    public Vector2 returnForceRange;
+    private int damage;
+    public void SetProperties(Vector2 range, Vector3 o, int h)
+    {
+        float scale = Random.Range(range.x, range.y);
+        health = h;
+        damage = (int)scale;
+        transform.localScale = Vector3.one * scale;
+        origin = o;
+    }
+    void Start()
+    {
+        returnForceRange = new Vector2(0,2);
+        rb = GetComponent<Rigidbody>();
+        bc = transform.parent.GetComponent<BoxCollider>();
+        velocityRange = transform.parent.GetComponent<AsteroidHandler>().velocityRange;
+        rb.AddForce(Random.Range(-velocityRange.x, velocityRange.y),
+            Random.Range(-velocityRange.x, velocityRange.y),
+            Random.Range(-velocityRange.x, velocityRange.y), ForceMode.Acceleration);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+        if (collision.gameObject.tag == "Bulelt") TakeDamge(collision.gameObject.GetComponent<Bullet>().damage);
+    }
+    private void Update()
+    {
+        if (Mathf.Abs(transform.position.x - transform.parent.position.x) > bc.size.x * bc.gameObject.transform.localScale.x
+            || Mathf.Abs(transform.position.y - transform.parent.position.y) > bc.size.y * bc.gameObject.transform.localScale.y
+            || Mathf.Abs(transform.position.z - transform.parent.position.z) > bc.size.z * bc.gameObject.transform.localScale.z)
+        {
+            rb.AddForce((transform.parent.position - transform.position)*Random.Range(returnForceRange.x,returnForceRange.y), ForceMode.Acceleration);
+        }
+    }
+    public void TakeDamge(int damage)=>health -= damage;
+}
