1dc907841b7bd3adad3fc9419f4e8ab0b093f51f
Connor <connorbp@hotmail.com>
2020-11-17 13:35:45 -0700

    Added first version of trails

---
+++ Assets/Scripts/ConnorTrail.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnorTrail : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.RotateAround(pos1.position, Vector3.forward, Time.deltaTime * speed);
+        transform.position += transform.up * Time.deltaTime * speed * -1;
+        
+    }
+}
