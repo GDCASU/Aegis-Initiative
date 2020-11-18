0896a3ed5f36cad96d600f39ec911d44602f96aa
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-11-18 15:00:18 -0700

    Basic history text

---
+++ Assets/Scripts/VisualNovelHandler.cs
@@ -0,0 +1,22 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualNovelHandler : MonoBehaviour
+{
+    public Text historyText;
+
+    private void Start()
+    {
+        historyText.text = "";
+    }
+
+    public void UpdateHistoryText()
+    {
+        SayDialog currentDialog = SayDialog.GetSayDialog();
+        string addText = string.Format("<b>{0}</b>\n{1}\n\n", currentDialog.NameText, currentDialog.StoryText);
+        historyText.text += addText;
+    }
+}
