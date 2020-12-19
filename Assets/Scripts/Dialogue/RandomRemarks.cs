fc563de5f815e017d6aab45c0ffb1aeba1de5ba9
Phyrol <nitehawkk123@gmail.com>
2020-12-18 18:04:12 -0700

    Random Remarks Basics
    - very basic initial setup
    - waiting on Christian to finalize co-pilot system

---
+++ Assets/Scripts/Dialogue/RandomRemarks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using Cinemachine;
+
+public class RandomRemarks : MonoBehaviour
+{
+    public Flowchart pilotFlowchart;
+
+    public string[] takeDamageRemarks;
+    public string[] collectPickUpsRemarks;
+    public string[] defeatEnemiesRemarks;
+    public string[] lostStageRemarks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(pilotFlowchart);
+    }
+}
