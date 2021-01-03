a813d6fb1e85962c1967e8c18ffa89c5010aadb2
Phyrol <nitehawkk123@gmail.com>
2021-01-02 20:17:04 -0700

    Fly Adult Setup
    - Fly Adult almost complete
    - movement/shooting
    - still need some comments and fine tunes

---
+++ Assets/Scripts/Enemies/SalivaBullet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//BULLET FOR SPACE FLY (ADULT)
+public class SalivaBullet : Bullet
+{
+    [SerializeField]
+    private float bulletSpeed = 0.10f;
+
+    private bool reachedDest = false;
+    Vector3 targetPos;
+
+    void Start()
+    {
+        Vector3 dir = transform.TransformDirection(targetPos - transform.localPosition);
+        transform.rotation = Quaternion.LookRotation(dir);
+        timer = bulletDespawnTime;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0) Destroy(transform.gameObject);
+
+        
+        if (Vector3.Distance(targetPos, transform.localPosition) < 0.01f) //reached target
+        {
+            //transform.SetParent(null); //remove from dolly
+            reachedDest = true; //don't allow it to continue using "MoveTowards"
+        }
+        else
+        {
+            transform.localPosition += transform.forward * bulletSpeed;
+
+            /*if(!reachedDest) //hasn't reached set targetPos yet
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, bulletSpeed);*/
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Player")) //hit player
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetTarget(Vector3 target) //set target position for this burst
+    {
+        targetPos = target;
+    }
+
+}
---
+++ Assets/Scripts/Enemies/SpaceFly_Adult.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFly_Adult : EnemyHealth
+{
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private int numBullets = 3;
+    [SerializeField]
+    private float shootTimerMin = 2.0f;
+    [SerializeField]
+    private float shootTimerMax = 6.0f;
+    private float shootTimer;
+    [SerializeField]
+    private float rateOfFire = 10f;
+
+    Vector3 playerPos;
+    private WaitForSeconds rof;
+
+    private void Start()
+    {
+        shootTimer = Random.Range(shootTimerMin, shootTimerMax);
+        rof = new WaitForSeconds(1/rateOfFire);
+    }
+
+    private void FixedUpdate()
+    {
+        if(shootTimer <= 0)
+        {
+            playerPos = GameObject.FindGameObjectWithTag("Player").transform.localPosition;
+            StartCoroutine(Shoot());
+            shootTimer = Random.Range(shootTimerMin, shootTimerMax);
+        }
+        else
+        {
+            shootTimer -= Time.deltaTime;
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        for(int i = 0; i < numBullets; i++)
+        {
+            GameObject temp = Instantiate(bullet, transform.position, Quaternion.identity, GameObject.Find("PlayerDollyCart").transform);
+            temp.GetComponent<SalivaBullet>().SetTarget(playerPos);
+
+            yield return rof;
+        }
+
+        yield return null;
+    }
+
+}
