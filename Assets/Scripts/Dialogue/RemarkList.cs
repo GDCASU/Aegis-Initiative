e5b4cb01ea37117a8220b497ca50d43d6b22c5f5
Phyrol <nitehawkk123@gmail.com>
2021-01-05 21:11:08 -0700

    Copilot Flowcharts
    - setup for random remarks
    - flowchart prefabs for copilots
    - some remark scripts (manager/copilot dialogue)

---
+++ Assets/Scripts/Dialogue/RemarkList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using Cinemachine;
+
+public class RemarkList : MonoBehaviour
+{
+    public Flowchart pilotFlowchart;
+
+    const int boxSize = 3;
+
+    [Header("Stage Remarks")]
+    [TextArea(boxSize, boxSize)]
+    public string[] enteringStage;
+    [TextArea(boxSize, boxSize)]
+    public string[] exitingStage;
+
+    [Header("Random Remarks")]
+    [TextArea(boxSize, boxSize)]
+    public string[] takingDamage;
+    [TextArea(boxSize, boxSize)]
+    public string[] collectingPickUps;
+    [TextArea(boxSize, boxSize)]
+    public string[] defeatingEnemies;
+    [TextArea(boxSize, boxSize)]
+    public string[] losingStage;
+
+    private void Start()
+    {
+        Instantiate(pilotFlowchart);
+    }
+}
---
+++ Assets/Scripts/Dialogue/RemarkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using Cinemachine;
+
+public class RemarkManager : MonoBehaviour
+{
+    private GameObject passivePilot;
+    private GameObject activePilot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
