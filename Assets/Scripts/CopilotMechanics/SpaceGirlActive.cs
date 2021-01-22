c96675be819bade584be530550ef2a32fa8cc660
mattcorey5 <70780360+mattcorey5@users.noreply.github.com>
2021-01-22 15:41:49 -0700

    Created Space Girl Copilot Prefab, Active, and Passive scripts
    -Created a prefab for the spacegirl copilot with the CopilotInfo script, Character script, as well as the 2 new scripts for spacegirl's active and passive abilities
    -Created a script for Spacegirl's active ability to heal to full one time per stage if at low health
    -Created a script for Spacegirl's passive ability to increase damage when at low health BUT it still needs to be updated with Christion D.'s new playerinfo script

---
+++ Assets/Scripts/CopilotMechanics/SpaceGirlActive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceGirlActive : CopilotActiveMechanic
+{
+    public float healthPercent = 25.0f;
+    public bool activeUsed;
+ 
+    private void Start()
+    {
+        activeUsed = false;
+    }
+    
+    private void Update()
+    {
+        if (PlayerHealth.singleton.health / PlayerHealth.singleton.maxHealth <= healthPercent / 100)
+        {
+            if (activeUsed == false && InputManager.GetButtonDown(PlayerInput.PlayerButton.ActiveAbility))
+            {
+                PlayerHealth.singleton.Heal(PlayerHealth.singleton.maxHealth);
+                activeUsed = true;
+            }
+        }
+    }
+
+    public override void CopyInfo(CopilotMechanic copilotMechanic)
+    {
+        base.CopyInfo(copilotMechanic);
+        healthPercent = ((SpaceGirlActive)copilotMechanic).healthPercent;
+        activeUsed = ((SpaceGirlActive)copilotMechanic).activeUsed;
+    }
+}
---
+++ Assets/Scripts/CopilotMechanics/SpaceGirlPassive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceGirlPassive : CopilotPassiveMechanic
+{
+    
+    public float healthPercent = 25.0f;
+    public int increasedDamage;
+    public GameObject bullets;
+
+    private int normalDamage;
+
+    private void Start()
+    {
+        normalDamage = bullets.GetComponent<Bullet>().damage;
+    }
+
+    private void Update()
+    {
+
+        if (PlayerHealth.singleton.health / PlayerHealth.singleton.maxHealth <= healthPercent / 100)
+            bullets.GetComponent<Bullet>().damage = increasedDamage;
+            else
+            bullets.GetComponent<Bullet>().damage = normalDamage;
+    }
+
+    public override void CopyInfo(CopilotMechanic copilotMechanic)
+    {
+        base.CopyInfo(copilotMechanic);
+    }
+}
