b77478f8a51361e943bf1aa2701048808680a150
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-01-22 23:47:51 -0800

    MushroomFriend Scripts
    Created MushroomFriendActive script
    Created MushroomFriendPassive script

---
+++ Assets/Scripts/CopilotMechanics/MushroomFriendActive.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomFriendActive : CopilotActiveMechanic
+{
+    //for testing
+    public float health; //to visualize when testing
+
+    //real stuff
+    public int heal; //amount to heal player
+    public float healTime; //time to restore health
+
+    public float sporeTimer;
+    public bool sporeStart;
+    private bool healPlayer;
+    private ParticleSystem spores;
+    private MeshRenderer bubbleMesh;
+
+    private Vector3 bubbleScale;
+    private bool bubbleInflated;
+
+    void Start()
+    {
+        healPlayer = false;
+        sporeStart = false;
+        spores = GetComponent<ParticleSystem>();
+        spores.Stop();
+        bubbleMesh = transform.GetChild(0).GetComponent<MeshRenderer>();
+        bubbleMesh.enabled = false;
+        bubbleInflated = false;
+
+        bubbleScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+    }
+
+    void Update()
+    {
+        //if (healPlayer)
+        //{
+        //    //if (InputManager.GetButtonDown(PlayerInput.PlayerButton.ActiveAbility))
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        sporeTimer = healTime;
+        //        spores.Simulate(0.0f, true, true); //reset particle system sporeTimer
+        //        spores.Play();
+        //    }
+        //}
+
+        if (healPlayer)
+        {
+            //if (InputManager.GetButtonDown(PlayerInput.PlayerButton.ActiveAbility))
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                bubbleMesh.enabled = true;
+            }
+        }
+
+        if (sporeStart)
+        {
+            sporeTimer = healTime;
+            spores.Simulate(0.0f, true, true); //reset particle system sporeTimer
+            spores.Play();
+
+            sporeStart = false;
+            print("start spores");
+        }
+
+        if (sporeTimer > 0)
+        {
+            sporeTimer -= Time.deltaTime;
+        }
+
+        if (sporeTimer < 0)
+        {
+            PlayerHealth.singleton.health = PlayerHealth.singleton.maxHealth; 
+            spores.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            healPlayer = false;
+            sporeTimer = 0;
+
+            bubbleMesh.transform.localScale = bubbleScale;
+            bubbleInflated = false;
+            bubbleMesh.enabled = false;
+        }
+
+        if (!bubbleInflated && bubbleMesh.enabled)
+        {
+            if (bubbleMesh.transform.localScale.x >= 0.7f)
+            {
+                sporeStart = true;
+                bubbleInflated = true;
+            }
+            else
+            {
+                bubbleMesh.transform.localScale += bubbleScale;
+            }
+        }
+
+        if (!healPlayer && PlayerHealth.singleton.health <= PlayerHealth.singleton.maxHealth * 0.25f)
+        {
+            healPlayer = true;
+        }
+    }
+
+    public override void CopyInfo(CopilotMechanic copilotMechanic)
+    {
+        base.CopyInfo(copilotMechanic);
+        heal = ((MushroomFriendActive)copilotMechanic).heal;
+        healTime = ((MushroomFriendActive)copilotMechanic).healTime;
+    }
+}
---
+++ Assets/Scripts/CopilotMechanics/MushroomFriendPassive.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomFriendPassive : CopilotPassiveMechanic
+{
+    //for testing
+    public int defenseIncrease; //amount to increase defense
+    public float sporeTimer; //lifetime of spores
+
+    //real stuff
+    public int healthLost;
+
+    public int health; //to detect if health decreased or increased
+
+    private bool defenseUp;
+    public List<float> spores;
+
+    void Start()
+    {
+        defenseUp = false;
+        health = PlayerHealth.singleton.health;
+        healthLost = (int)(PlayerHealth.singleton.health * 0.1f);
+        spores = new List<float>();
+    }
+
+    void Update()
+    {
+        if (PlayerHealth.singleton.health > health)
+        {
+            health = PlayerHealth.singleton.health;
+        }
+
+        if (spores.Count >= 1)
+        {
+            for (int index = 0; index < spores.Count; index++)
+            {
+                {
+                    spores[index] -= Time.deltaTime;
+                }
+            }
+            spores.RemoveAll(EndTimer);
+            PlayerHealth.singleton.defense = spores.Count*defenseIncrease;
+        }
+
+        if (defenseUp)
+        {
+            spores.Add(sporeTimer);
+            health = PlayerHealth.singleton.health;
+            defenseUp = false;
+        }
+
+        if (!defenseUp && PlayerHealth.singleton.health < health && PlayerHealth.singleton.health % healthLost == 0)
+        {
+            defenseUp = true;
+        }
+    }
+
+    private bool EndTimer(float time)
+    {
+        return time <= 0;
+    }
+
+    public override void CopyInfo(CopilotMechanic copilotMechanic)
+    {
+        base.CopyInfo(copilotMechanic);
+        defenseIncrease = ((MushroomFriendPassive)copilotMechanic).defenseIncrease;
+    }
+}
