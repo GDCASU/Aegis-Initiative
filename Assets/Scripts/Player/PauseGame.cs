673088c4c31f0defdc6cc57e75f82a16e19ec643
Thy Do <thydo@asu.edu>
2021-02-04 14:04:37 -0700

    Added pause game script

---
+++ Assets/Scripts/Player/PauseGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool isPaused = false;
+    public bool Esc = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.GetButton(PlayerInput.PlayerButton.Pause))
+        {
+            isPaused = !isPaused;
+            Pause();
+        }
+        Esc = InputManager.GetButton(PlayerInput.PlayerButton.Pause);
+    }
+
+    void Pause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
