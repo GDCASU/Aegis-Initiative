992200732644f8500f1646a8c62c846cb6286aed
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-02-05 15:55:26 -0800

    Updated and Moved Pufferbomb Script
    -Fixed bug that would continuously damage Player when Player is close to Pufferbomb
    -Fixed bug when Player shoots Pufferbomb and Player gets damaged
    -Moved Pufferbomb Script from Environment to Enemies

--- Assets/Scripts/Enemies/Pufferbomb.cs
+++ Assets/Scripts/Enemies/Pufferbomb.cs
@@ -10,42 +10,43 @@ public class Pufferbomb : MonoBehaviour
     private ParticleSystem explosion;
     private MeshRenderer mesh;
     private bool isExploding; //check that Pufferbomb is already exploding
+    private bool isShot; //check if Player already shot Pufferbomb
 
     void Start()
     {
-        explosion = this.transform.GetComponent<ParticleSystem>();
-        mesh = this.transform.GetComponent<MeshRenderer>();
+        explosion = transform.GetComponent<ParticleSystem>();
+        mesh = transform.GetComponent<MeshRenderer>();
+
+        isExploding = false;
+        isShot = false;
     }
 
     void Update()
     {
-        if (isExploding)
-        {
-            PlayerInfo.singleton.TakeDamage(damage); //damage to Player
-            Destroy(this.gameObject);
-        }
-
-        if (!isExploding && Vector3.Distance(PlayerInfo.singleton.transform.position, transform.position) <= distance)
+        if (!isExploding && !isShot && Vector3.Distance(PlayerInfo.singleton.transform.position, transform.position) <= distance)
         {
             StartCoroutine(Explode()); //start explode coroutine 
+            PlayerInfo.singleton.TakeDamage(damage); //damage Player
         }
     }
 
     //Play explosion particle system effect
     private IEnumerator Explode()
     {
+        isExploding = true;
         mesh.enabled = false;
         explosion.Play(); //start Particle System explosion effect
         yield return new WaitForSeconds(1.2f); //wait for explosion to finish before destroying Pufferbomb
-        isExploding = true;
+        Destroy(gameObject);
     }
 
     //If Player shoots Pufferbomb, destroy Pufferbomb
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Bullet")
+        if (!isExploding && collision.gameObject.CompareTag("Bullet"))
         {
-            StartCoroutine(Explode()); //start explode coroutine
+            isShot = true;
+            StartCoroutine(Explode()); //start explode coroutine  
         }
     }
 }
