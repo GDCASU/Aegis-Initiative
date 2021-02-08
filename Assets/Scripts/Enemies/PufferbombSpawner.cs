3366c6ea22531d1c88500443093a7bb022e86722
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-02-08 13:41:17 -0800

    Created PufferbombSpawner Script
    -Pufferbombs will float up at a given height and speed
    -Height - how far up the Pufferbombs will move relative to the initial y position
    - Speed - how fast the Pufferbombs will move

---
+++ Assets/Scripts/Enemies/PufferbombSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PufferbombSpawner : MonoBehaviour
+{
+    public float height; //how far up to float Pufferbombs from initial y position
+    public float speed; //speed to float Pufferbombs
+
+    private bool floatUp;
+    private float finalHeight;
+
+    void Start()
+    {
+        //disable Pufferbomb script for all Pufferbombs
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<Pufferbomb>() != null)
+            {
+                child.GetComponent<Pufferbomb>().enabled = true;
+            }
+        }
+
+        floatUp = false;
+        finalHeight = transform.position.y + height;
+    }
+
+    void Update()
+    {
+        //Move the Pufferbombs up at the given speed
+        if (floatUp && (transform.position.y < finalHeight))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+    }
+
+    //When the player collides with the PufferbombSpawner, then enable the Pufferbomb Script of all the Pufferbombs
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponent<Pufferbomb>() != null)
+                {
+                    child.GetComponent<Pufferbomb>().enabled = true;
+                }
+            }
+            floatUp = true;
+        }
+    }
+}
