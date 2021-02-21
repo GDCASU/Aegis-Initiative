98d0d769cae6657a8089d828e9d3d8189c52fc43
Ajai <ajai.gorowara@gmail.com>
2021-02-21 15:08:31 -0700

    Added enemies & underwater filter

---
+++ Assets/UnderwaterFilter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterFilter : MonoBehaviour
+{
+
+	public GameObject filter;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			if (filter.activeSelf == false)
+			{
+				filter.SetActive(true);
+			}
+			else
+			{
+				filter.SetActive(false);
+			}
+		}
+	}
+}
