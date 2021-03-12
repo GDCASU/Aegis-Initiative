3ec0b5aeeefa093cf6e799fd42aaae6998904b39
Thy Do <thydo@asu.edu>
2021-03-12 03:31:05 -0700

    Renamed slugrockthrower to ProjectileAttack
    created canonball that inherits from ProjectileAttack and spawns rocks. although from the sky, for now

--- Assets/Scripts/Enemies/ProjectileAttack.cs
+++ Assets/Scripts/Enemies/ProjectileAttack.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlugRockThrower : MonoBehaviour
+public class ProjectileAttack : MonoBehaviour
 {
     public GameObject rockPrefab;
     public CinemachineSmoothPath path;
