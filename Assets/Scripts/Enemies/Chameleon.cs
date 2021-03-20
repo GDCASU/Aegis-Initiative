46717305323e25ff01861b20ee411dc297955827
propredator23 <christiandash@hotmail.com>
2021-03-20 02:21:02 -0700

    Chameleon.cs and prefab update
    -Made the Chameleon script derive from enemyHealth to only need one component on the prefab and also handle collisions correctly

--- Assets/Scripts/Enemies/Chameleon.cs
+++ Assets/Scripts/Enemies/Chameleon.cs
@@ -11,7 +11,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chameleon : MonoBehaviour
+public class Chameleon : EnemyHealth
 {
     [SerializeField]
     [Range(0f, 1f)]
