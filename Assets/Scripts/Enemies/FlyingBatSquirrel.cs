d237eac79e33f094dc036865f06ae8605942e5d3
Voidijure <cristiondominguez@gmail.com>
2021-03-26 14:55:46 -0700

    Flying Bat-Squirrel Final Draft
    Flying Bat-Squirrel has a vertical PlayerDetection collider box that disables the Squirrel's following capability after collision.

--- Assets/Scripts/Enemies/FlyingBatSquirrel.cs
+++ Assets/Scripts/Enemies/FlyingBatSquirrel.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/*
+ * Rotates Flying Bat-Squirrel around Host on a horizontal axis whilst moving Host towards the Player until it passes the Player.
+ * 
+ * Author: Cristion Dominguez
+ * Date: 26 March 2021
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,23 +23,48 @@ public class FlyingBatSquirrel : MonoBehaviour
     [Tooltip("Speed at which Squirrel shall spin around its host.")]
     private float spinSpeed = 700f;
 
+    [SerializeField]
+    [Tooltip("Does the Squirrel spawn in the Player Dolly Cart?")]
+    private bool spawnsInDollyCart = true;
+
     private Transform host, player;
+    private bool isFollowingPlayer = true;  // Is the Squirrel (Host) following the Player?
 
-    Vector3 targetDirection, newDirection;
+    Vector3 targetDirection, newDirection;  // for rotation calculation
 
+    /// <summary>
+    /// Initializes variables and turns the Flying Bat-Squirrel around towards the Player's general direction if it spawns in Player Dolly Cart (Cart orients enemies away from the Player).
+    /// </summary>
     private void Start()
     {
         host = transform.parent;
         player = PlayerInfo.singleton.transform;
+
+        if (spawnsInDollyCart)
+        {
+            float yAngles = host.localEulerAngles.y;
+            if (yAngles < 90 || yAngles > 270)
+                yAngles += 180;
+            host.localEulerAngles = new Vector3(host.localEulerAngles.x, yAngles, host.localEulerAngles.z);
+        }
     }
 
+    /// <summary>
+    /// Rotates Squirrel around Host and translates Host forwards. If the Squirrel is following the Player, then the Host rotates towards the Player.
+    /// </summary>
     private void Update()
     {
-        transform.RotateAround(host.position, host.up, spinSpeed * Time.deltaTime);
-        host.eulerAngles = RotateToObject(player);
+        if (isFollowingPlayer)
+            host.eulerAngles = RotateToObject(player);
+
+        transform.RotateAround(host.position, host.forward, spinSpeed * Time.deltaTime);
         host.Translate(Vector3.forward * approachSpeed * Time.deltaTime);
     }
 
+    /// <summary>
+    /// Deals damage to Player upon collision between Squirrel and Player.
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag.Equals("Player"))
@@ -54,4 +86,9 @@ public class FlyingBatSquirrel : MonoBehaviour
         newDirection = Vector3.RotateTowards(host.forward, targetDirection, followIntensity * Time.deltaTime, 0);
         return Quaternion.LookRotation(newDirection).eulerAngles;
     }
+
+    /// <summary>
+    /// Notifies Host to stop following the Player.
+    /// </summary>
+    public void StopFollowingPlayer() => isFollowingPlayer = false;
 }
