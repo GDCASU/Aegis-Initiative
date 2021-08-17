9fd417f5f281b2c5684f7b12fda62dea2b3cd611
Christian Gonzalez <christiandash@hotmail.com>
2021-08-16 17:15:34 -0700

    Intro Scene Added

--- Assets/Scripts/Dialogue/VNSceneManager.cs
+++ Assets/Scripts/Dialogue/VNSceneManager.cs
@@ -16,20 +16,23 @@ public class VNSceneManager : MonoBehaviour
 
         copilotName = SceneManager.GetActiveScene().name;
 
-        int vnNumber = GameManager.singleton.activeCopilot.GetComponent<CopilotInfo>().copilotData.vnScenesCompleted;
-        //print(flowchart.GetComponent<SetupFlowchart>().sceneFlowchart.FindBlock(copilotName + vnNumber));
-        //if dialogue box exists, execute it
-        if (flowchart.FindBlock(copilotName + vnNumber))
+        if (SceneManager.GetActiveScene().name != "Intro")
         {
-            //execute block with copilotname and current level of copilot
-            flowchart.ExecuteBlock(copilotName + vnNumber);
-        }
-        else
-        {
-            //player has no VN scenes left
-            //This should save the profile then load the next gameplay scene
-            profileManager.SaveCurrentProfile();
-            SceneManager.LoadScene(GameManager.singleton.levels[profileManager.CurrentProfile.currentStage]);
+            int vnNumber = GameManager.singleton.activeCopilot.GetComponent<CopilotInfo>().copilotData.vnScenesCompleted;
+            //print(flowchart.GetComponent<SetupFlowchart>().sceneFlowchart.FindBlock(copilotName + vnNumber));
+            //if dialogue box exists, execute it
+            if (flowchart.FindBlock(copilotName + vnNumber))
+            {
+                //execute block with copilotname and current level of copilot
+                flowchart.ExecuteBlock(copilotName + vnNumber);
+            }
+            else
+            {
+                //player has no VN scenes left
+                //This should save the profile then load the next gameplay scene
+                profileManager.SaveCurrentProfile();
+                SceneManager.LoadScene(GameManager.singleton.levels[profileManager.CurrentProfile.currentStage]);
+            }
         }
     }
 
@@ -38,10 +41,12 @@ public class VNSceneManager : MonoBehaviour
     public void EndVNScene()
     {
         //update this pilots completed VNscenes by 1
-        GameManager.singleton.activeCopilot.GetComponent<CopilotInfo>().copilotData.vnScenesCompleted += 1;
-
-        GameManager.singleton.SaveProgress();
-        profileManager.SaveCurrentProfile();
+        if (SceneManager.GetActiveScene().name != "Intro")
+        {
+            GameManager.singleton.activeCopilot.GetComponent<CopilotInfo>().copilotData.vnScenesCompleted += 1;
+            GameManager.singleton.SaveProgress();
+            profileManager.SaveCurrentProfile();
+        }
 
         //This should load the next gameplay level
         SceneManager.LoadScene("CopilotUI");
