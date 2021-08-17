3728996526b67c1e336ac190826bc33360230074
Christian Gonzalez <christiandash@hotmail.com>
2021-08-16 20:02:24 -0700

    UI Changes

--- Assets/Scripts/Player/SimpleCameraController.cs
+++ Assets/Scripts/Player/SimpleCameraController.cs
@@ -119,13 +119,13 @@ namespace UnityTemplateProjects
 #if ENABLE_LEGACY_INPUT_MANAGER
 
             // Exit Sample  
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
-				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
-				#endif
-            }
+    //        if (Input.GetKey(KeyCode.Escape))
+    //        {
+    //            Application.Quit();
+				//#if UNITY_EDITOR
+				//UnityEditor.EditorApplication.isPlaying = false; 
+				//#endif
+    //        }
             // Hide and lock cursor when right mouse button pressed
             if (Input.GetMouseButtonDown(1))
             {
