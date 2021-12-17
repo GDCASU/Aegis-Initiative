0c82cf91a43f077b608b1298633fd42567c46f01
Phyrol <bpotzka@gmail.com>
2021-12-16 19:20:19 -0700

    Reticle jitter fixed
    - disable CM VCam on start
    - reticle still jitters about 1 pixel but barely an issue
    - disabled Player Cam on each scene

---
+++ Assets/Scripts/DisableOnStart.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnStart : MonoBehaviour
+{
+    private void Start() => gameObject.SetActive(false);
+}
