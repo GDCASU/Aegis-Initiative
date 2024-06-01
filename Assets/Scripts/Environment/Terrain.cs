9b2322e5139132af2d712e136ea3879cb1cc13f6
Bradley Potzka <bpotzka@amazon.com>
2024-05-31 17:33:16 -0700

    implement damage from terrain
    - create a script to apply to Terrain that checks for collision with player

---
+++ Assets/Scripts/Environment/Terrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerInfo>().TakeDamage(5);
+        }
+    }
+}
