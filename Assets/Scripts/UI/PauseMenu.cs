1e0c4541f888369e960cdc94a04ac9c305ea8dbe
Bradley Potzka <bpotzka@amazon.com>
2024-05-31 20:00:59 -0700

    remove debug statements

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -202,9 +202,7 @@ public class PauseMenu : MonoBehaviour
 
     public void SwitchControllerType(Dropdown change)
     {
-        Debug.Log("value: " + change.value);
         InputManager.inputMode = (InputManager.InputMode)change.value;
-        Debug.Log("after: " + InputManager.inputMode);
     }
     public void SwitchPanels(int panelToActivate)
     {
