e202361e52cfcb57f7d9104d85f2acc9e40ffd9d
Bradley Potzka <bpotzka@amazon.com>
2024-06-04 19:30:33 -0700

    chore: adjust where the logic lives to unlock co pilots

--- Assets/Scripts/Enemies/AsteroidBoss/AsteroidBoss.cs
+++ Assets/Scripts/Enemies/AsteroidBoss/AsteroidBoss.cs
@@ -45,7 +45,6 @@ public class AsteroidBoss : EnemyHealth
 
     public override void DestroyEnemy()
     {
-        GameManager.singleton.AddCopilot(2);
         PlayerInfo.singleton.GetComponent<StageTriggers>().EndLevel();
         base.DestroyEnemy();
     }
